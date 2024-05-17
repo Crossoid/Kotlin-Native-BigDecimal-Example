c7958fbdc61cf778340326dda11dfaa72e029b80
Jan Holesovsky <kendy@crossoid.com>
2024-05-17 11:11:24 +0200

    Use BigDecimal in commonMain
    This compiles and works on Android, but fails on iOS.
    
    With this, you will see the following on Android:
    
      Hello, Android XY!
      I can count with BigDecimal: 5 * 8 = 40

--- shared/src/commonMain/kotlin/com/crossoid/bigdecimal_example/Greeting.kt
+++ shared/src/commonMain/kotlin/com/crossoid/bigdecimal_example/Greeting.kt
@@ -1,9 +1,15 @@
 package com.crossoid.bigdecimal_example
 
+import java.math.BigDecimal
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        val first = BigDecimal("5")
+        val second = BigDecimal("8")
+        val result = "I can count with BigDecimal: $first * $second = ${first*second}"
+
+        return "Hello, ${platform.name}!\n$result"
     }
 }
